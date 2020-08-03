e16a0ba0ab4cde0d25972eee5588380b96dfc8df
Andre2610 <dre2610@gmail.com>
2020-08-03 20:15:34 +0200

    content and picture on tripdetailscard fixed

--- src/Components/TripCard.tsx
+++ src/Components/TripCard.tsx
@@ -17,7 +17,7 @@ export default function TripCard(props: Trip) {
           <Heading as="h3" size="sm" w="95%" m="auto" mt="1rem" mb="0.5rem">
             {title}
           </Heading>
-          <Box w="95%">
+          <Box w="45%">
             {paragraphs.map((paragraph, i) => (
               <Text key={i} my="0.5rem">
                 {paragraph}
@@ -50,7 +50,7 @@ export default function TripCard(props: Trip) {
     history.push(`/trip/${id}`);
   }
   return (
-    <>
+    <Box key={id}>
       <Heading className="tripTitle" textAlign="center">
         {tripTitle}
       </Heading>
@@ -65,10 +65,10 @@ export default function TripCard(props: Trip) {
             .map((post: Post) => {
               const { id, title, content, pictures } = post;
               return (
-                <>
-                  <Box key={id}>{postsRender(title, content, pictures)}</Box>
+                <Box key={id}>
+                  <Box>{postsRender(title, content, pictures)}</Box>
                   <Divider borderColor="gray.500" py={2} />
-                </>
+                </Box>
               );
             })
         : null}
@@ -82,6 +82,6 @@ export default function TripCard(props: Trip) {
           Explore this trip!
         </Button>
       </Flex>
-    </>
+    </Box>
   );
 }
